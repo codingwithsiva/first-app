889369dd315a574da361dec12f413ee58a61230e
codingwithsiva <sivareddykota007@gmail.com>
2019-09-18 21:40:23 +0530

    git commit -m "fitst commit"

---
+++ logger.js
@@ -0,0 +1,16 @@
+const EventEmitter = require('events');
+
+var url = "http://code.com/api/";
+
+class Logger extends EventEmitter {
+    log(message) {
+        console.log(message);
+        this.emit('messageLogged', { id: 1, url: "http://dsfa.com" });
+    }
+
+    register(value) {
+        console.log(value);
+    }
+}
+
+module.exports = Logger;
